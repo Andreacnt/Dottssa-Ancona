51279e744661cb577e8d159459ebbae5ab34d431
Andreacnt <andreaconti495@gmail.com>
2026-09-22 12:30:54 +0200

    menu: logo size large nativo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-st.logo("img/logo-bar.png")
+st.logo("img/logo-bar.png", size="large")
 
 st.markdown(CUSTOM_CSS, unsafe_allow_html=True)
 
